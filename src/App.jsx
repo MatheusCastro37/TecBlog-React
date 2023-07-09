4b361b2e5931a90df39305dded5b9a595df7f3d9
Matheus Castro <mate.cas37@gmail.com>
2023-07-09 17:04:54 -0300

    criado a parte lateral e o rodape.

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,34 @@ function App() {
           <ButtonHeader title="ELETRONICOS" />
         </nav>
       </Header>
-      <CardContainer />
-      <CardContainer />
+      <div>
+        <main>
+          <CardContainer />
+          <CardContainer />
+        </main>
+        <div id='sideContent'>
+          <aside>
+            <h3>Postagens recentes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minima nisi iste error, tempora adipisci cupiditate a possimus repellendus. Ipsa, perferendis molestiae possimus ipsum nesciunt non. Voluptate cupiditate perferendis expedita?</p>
+            <a href="#" >Leia mais...</a>
+            <hr/>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo suscipit totam cum aperiam? Ex necessitatibus quisquam quaerat nemo repudiandae commodi eius rerum dolorem earum, molestias fuga assumenda quo perspiciatis eum!</p>
+            <a href="#" >Leia mais...</a>
+          </aside>
+
+          <aside id='content'>
+            <h3>Conteudo</h3>
+            <a href="#">Jogos</a>
+            <a href="#">Celulares</a>
+            <a href="#">Informatica</a>
+            <a href="#">Eletronicos</a>
+            <a href="#">Computadores</a>
+          </aside>
+        </div>
+      </div>
+      <footer>
+        <p>Todos os direitos resevados</p>
+      </footer>
     </div>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.header`
     display: flex;
     justify-content: center;
-    width: 100vw;
+    width: 100%;
     height: 140px;
     background-color: rgb(247, 182, 0);
     flex-direction: column;
